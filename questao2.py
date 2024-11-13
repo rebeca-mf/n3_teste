1e1a0adea475b355097d23644e6772a68198259a
rebeca-mf <rebecamf_@hotmail.com>
2024-11-13 11:41:03 -0300

    questao2.py

--- questao2.py
+++ questao2.py
@@ -6,3 +6,29 @@ st.markdown("Este é texto")
 st.caption("Esta é a a legenda")
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+'''Informar como colher os dados através de variáveis'''
+x = st.checkbox('Sim')
+st.write(x)
+
+# fazer o mesmo com alguns dos comandos abaixo
+st.title(x)
+st.button('Clique')
+st.radio('Selecione seu gênero',['Masculino','Feminino'])
+st.selectbox('Selecione seu gênero',['Masculino','Feminino'])
+st.multiselect('Escolha um departamento',['DCS', 'DE', 'DIR'])
+st.select_slider('Selecione uma resposta', ['Ruim', 'Bom', 'Excelente'])
+st.slider('Selecione um número', 0,50)
+st.number_input('Selecione um número', 0,10)
+st.text_input('Endereço de e-mail')
+st.date_input('Data de viagem')
+st.time_input('Tempo de escola')
+st.text_area('Descrição')
+st.file_uploader('Atualize uma foto')
+st.color_picker('Escolha sua cor favorita')
+
+# mensagens de status
+st.success("Você conseguiu!")
+st.error("Erro!")
+st.warning("Advertência")
+st.info("Esta é uma informação")
